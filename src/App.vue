8df4ea0a94bb822b0b9c020c54e4ab5d6271952f
Michele Arcopinto <150271708+michelearcopinto@users.noreply.github.com>
2024-02-09 15:48:18 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import ExampleName from "./components/ExampleName.vue";
+import { store } from "./store";
+import axios from "axios";
+
+export default {
+  components: {
+    ExampleName,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  created() {
+    this.requestRandomMail();
+  },
+  methods: {
+    requestRandomMail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          store.randomMail = result.data.response;
+        });
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <h2>{{ store.helloValue }}</h2>
+  <h3>axios request: {{ store.randomMail }}</h3>
+  <ExampleName />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+h2 {
+  font-size: 120px;
+  color: aquamarine;
+}
+
+h3 {
+  font-size: 80px;
+  color: blueviolet;
+}
+</style>
